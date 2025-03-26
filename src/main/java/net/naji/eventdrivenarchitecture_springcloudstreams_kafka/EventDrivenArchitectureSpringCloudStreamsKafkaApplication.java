7c8eecba4ec1bdc2cc3375430837b494beea07c8
naji <hafidanaji13@gmail.com>
2025-03-26 13:47:00 +0000

    comment installer KAFKA

---
+++ src/main/java/net/naji/eventdrivenarchitecture_springcloudstreams_kafka/EventDrivenArchitectureSpringCloudStreamsKafkaApplication.java
@@ -0,0 +1,13 @@
+package net.naji.eventdrivenarchitecture_springcloudstreams_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenArchitectureSpringCloudStreamsKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventDrivenArchitectureSpringCloudStreamsKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/naji/eventdrivenarchitecture_springcloudstreams_kafka/EventDrivenArchitectureSpringCloudStreamsKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.naji.eventdrivenarchitecture_springcloudstreams_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenArchitectureSpringCloudStreamsKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
